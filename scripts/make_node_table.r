eb525b4568ec2647ee5b07405674baa8aadcde13
Defne <defnesurujon@gmail.com>
2019-06-26 15:16:57 -0400

    Update make_node_table.r

--- scripts/make_node_table.r
+++ scripts/make_node_table.r
@@ -2,12 +2,12 @@ library(igraph)
 
 make_node_table<-function(edgetable){
   #make new nodes table from edges
-  nodes<-as.character(unique(c(edgetable['source'],edgetable['target'])))
+  nodes<-unique(c(edgetable$source,edgetable$target))
   nodeIDs<-c(1:(length(nodes)))
   nodetable<-data.frame("label"=nodes,"id"=nodeIDs)
   
   #add network stats
-  mynet <- graph_from_edgelist(as.matrix(edges[,c('source','target')]))
+  mynet <- graph_from_edgelist(as.matrix(edgetable[,c('source','target')]))
   nodetable['Degree'] <- degree(mynet, v=nodetable$label)
   nodetable['Betweenness'] <- betweenness(mynet, v=nodetable$label)
   eigencen <- eigen_centrality(mynet)$vector
